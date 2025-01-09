9952cff739246ab6cd936caee547691ef32d1e9a
Elaine <elaine.li@u.yale-nus.edu.sg>
2025-01-09 18:42:41 -0500

    added unreach optimization, added notes about performance

--- query-generator/scc.ml
+++ query-generator/scc.ml
@@ -8,6 +8,13 @@ open Unreach
 let filter_transitions_scc_participant (ls: symbolic_transition list) (p: participant) =
    filter_by_sender ls p 
 
+let filter_participant_pairs_for_unreach (prot: symbolic_protocol) (p: participant) = 
+	(* Defined like so to avoid repeats *)
+	let all_pairs = all_distinct_participant_pairs prot in 
+	let relevant_transitions = filter_transitions_scc_participant prot.transitions p in 
+	let relevant_pairs = List.map (fun tr -> (tr.sender, tr.receiver)) relevant_transitions in 
+	intersection all_pairs relevant_pairs 
+
 (* For each transition, pair it with each state in the protocol and generate four conjuncts *)
 let first_conjunct_from_transition (prot: symbolic_protocol) (tr: symbolic_transition) (p: participant) = 
 	"(" ^ 
@@ -63,16 +70,16 @@ let generate_scc_preamble_for_participant (prot: symbolic_protocol) (p: particip
   List.fold_left (fun acc tr -> acc ^ "\n\\/\n" ^ generate_scc_body_from_transition_for_participant prot tr p) "false" transitions ^ 
 	"\ns.t.\n"
 
-let generate_unreach (prot: symbolic_protocol) : string = 
-	let bs_list = all_distinct_participant_pairs prot in 
-	List.fold_left (fun acc (p,q) -> acc ^ "\n" ^ unreach_for_participant_pair prot p q) "" bs_list
+let generate_unreach_for_participant (prot: symbolic_protocol) (p: participant) : string = 
+	let pairs_list = filter_participant_pairs_for_unreach prot p in 
+	List.fold_left (fun acc (p,q) -> acc ^ "\n" ^ unreach_for_participant_pair prot p q) "" pairs_list
 
 let generate_scc_for_participant (prot: symbolic_protocol) (p: participant) = 
 	generate_scc_preamble_for_participant prot p ^ 
 	"\n" ^ 
 	generate_prodreach_for_participant prot p ^ 
 	"\n\n" ^ 
-	generate_unreach prot 
+	generate_unreach_for_participant prot p 
 
 let generate_scc_filename_for_participant (p: participant) = 
 	p ^ "_scc.hes" 
